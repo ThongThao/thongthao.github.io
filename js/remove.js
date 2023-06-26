ec5fd15a06769db0c4f8c1f730de8a5582b87ff9
ThongThao <thaotran4135@gmail.com>
2023-06-26 23:00:17 +0700

    add

---
+++ js/remove.js
@@ -0,0 +1,29 @@
+$(document).ready(function(c) {
+    $('.close1').on('click', function(c) {
+      $(this).closest('tr').fadeOut('slow', function(c) {
+        $(this).remove();
+      });
+    });
+  });
+  $(document).ready(function(c) {
+    $('.close2').on('click', function(c) {
+      $(this).closest('tr').fadeOut('slow', function(c) {
+        $(this).remove();
+      });
+    });
+  });
+  $(document).ready(function(c) {
+    $('.close3').on('click', function(c) {
+      $(this).closest('tr').fadeOut('slow', function(c) {
+        $(this).remove();
+      });
+    });
+  });
+  $(document).ready(function(c) {
+    $('.close4').on('click', function(c) {
+      $(this).closest('tr').fadeOut('slow', function(c) {
+        $(this).remove();
+      });
+    });
+  });
+  
